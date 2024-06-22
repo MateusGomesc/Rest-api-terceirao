7962f597dd68c66df77f3a9ecd360887b8318c02
Mateus Gomes <mateuscostagomes000@gmail.com>
2024-06-22 15:56:42 -0300

    fix

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const mysql = require('mysql2')
-
-const urlDB = "mysql://root:vUQiOsdbLoXKNlpoJsuzepEferWQbloK@roundhouse.proxy.rlwy.net:18905/railway"
-
-const connection = mysql.createConnection(urlDB)
-
-module.exports = connection
